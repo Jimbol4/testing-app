c987cd48cd6ecb1944ddb7aef420a7f5aee66679
vagrant <vagrant@homestead>
2016-02-29 19:17:10 +0000

    Email testing added

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,15 +6,34 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+    
+    use MailTracking;
+     
     /**
      * A basic functional test example.
      *
      * @return void
      */
     public function testBasicExample()
-    {
-        $this->visit('/')
-             ->see('Laravel 5.2');
+    {   
+        Mail::raw('Hello world', function($message){
+            $message->to('foo@bar.com');
+            $message->from('bar@foo.com');
+        });
+        
+        $this->seeEmailWasSent();
+        
+        Mail::raw('Hello world', function($message){
+            $message->to('foo@bar.com');
+            $message->from('bar@foo.com');
+            $message->subject('Bleh');
+        });
+        
+        $this->seeEmailsSent(2)
+             ->seeEmailTo('foo@bar.com')
+             ->seeEmailFrom('bar@foo.com')
+             ->seeEmailSubjectIs('Bleh');
+        
     }
     
     /**
@@ -28,4 +47,6 @@ class ExampleTest extends TestCase
              ->see('You\'ve been clicked, punk.')
              ->seePageIs('/feedback');
     }
+    
+
 }
